b54ca4947b4f6b0347f17f915ab3253890892556
unknown <rmtestard@gmail.com>
2019-05-15 20:39:31 +0200

    cube rotation

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(3, 3, 3);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
